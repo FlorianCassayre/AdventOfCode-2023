8fa9a8d61c028629a1e2cd33f9dcacac8aa71319
Florian Cassayre <florian.cassayre@gmail.com>
2023-12-01 10:22:22 +0100

    Solved day 01

---
+++ src/main/scala/adventofcode/solutions/Day01.scala
@@ -0,0 +1,17 @@
+package adventofcode.solutions
+
+import adventofcode.Definitions.*
+
+@main def Day01 = Day(1) { (input, part) =>
+
+  def sum(table: String => Option[Int]) = input.toLines.map(_.tails.flatMap(table).toSeq).map(r => s"${r.head}${r.last}".toInt).sum
+
+  def asDigit(s: String) = s.headOption.filter(_.isDigit).map(_.asDigit)
+
+  part(1) = sum(asDigit)
+
+  val digits = Seq("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+  part(2) = sum(s => asDigit(s).orElse(Some(digits.indexWhere(d => s.startsWith(d)) + 1).filter(_ > 0)))
+
+}
